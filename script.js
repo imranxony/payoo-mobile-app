ba662e2100fa59a717d86d7abcfc6108b93c890e
imranxony <amran584044@gmail.com>
2025-08-23 11:42:13 +0100

    Login page done

---
+++ script.js
@@ -0,0 +1,22 @@
+// Login button functionality
+document.getElementById("loginButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNumber = 222989177;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+  console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid credincials");
+  }
+});
